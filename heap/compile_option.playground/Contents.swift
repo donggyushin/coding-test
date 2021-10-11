7d1db3c041462043c5cd7ff7a2db96874fbc80e4
신동규 <donggyu9410@gmail.com>
2021-10-11 20:28:01 +0900

    컴파일 옵션

---
+++ heap/compile_option.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ jobs:[[Int]]) -> Int {
+    return 0
+}
+
+func test() {
+    guard solution([[0, 3], [1, 9], [2, 6]]) == 9 else { return print("test fail ")}
+    print("test success")
+}
+
+test()
