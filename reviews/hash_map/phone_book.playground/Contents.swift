9c751daf713b4ae9799815a3628d00219c0a4fce
신동규 <donggyu9410@gmail.com>
2021-10-30 22:17:42 +0900

    전화번호부

--- reviews/hash_map/phone_book.playground/Contents.swift
+++ reviews/hash_map/phone_book.playground/Contents.swift
@@ -1,31 +1,12 @@
 import Foundation
 
 func solution(_ phone_book: [String]) -> Bool {
-    var answer = true
-    
-    func dfs(left: String, rights: [String]) {
-        if rights.isEmpty { return }
-        var rights = rights
-        let right = rights.removeFirst()
-        if left.contains(right) || right.contains(left) { return answer = false }
-        else { dfs(left: left, rights: rights) }
-    }
-    
-    var phone_book = phone_book
-    
-    while phone_book.isEmpty == false {
-        if answer == false { return false }
-        let left = phone_book.removeFirst()
-        dfs(left: left, rights: phone_book)
+    let phone_book = phone_book.sorted()
+    for (p1, p2) in zip(phone_book, phone_book[1..<phone_book.count]) {
+        if p2.starts(with: p1) {
+            return false
+        }
     }
     
     return true
 }
-
-func test() {
-    print(solution(["119", "97674223", "1195524421"]), false)
-    print(solution(["123","456","789"]), true)
-    print(solution(["12","123","1235","567","88"]), false)
-}
-
-test()
