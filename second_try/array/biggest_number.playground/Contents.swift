d0cc8db9ffc1c29756eafe1f2b6302803671f20a
신동규 <donggyu9410@gmail.com>
2021-11-06 00:09:39 +0900

    가장 큰 수

--- second_try/array/biggest_number.playground/Contents.swift
+++ second_try/array/biggest_number.playground/Contents.swift
@@ -2,5 +2,5 @@ import Foundation
 
 func solution(_ numbers:[Int]) -> String {
     let numbers = numbers.map({ String($0) }).sorted(by: { $0 + $1 > $1 + $0 })
-    return numbers.filter({ $0 != "0" }).count == 0 ? "0" : numbers.joined(separator: "")
+    return numbers[0] == "0" ? "0" : numbers.joined(separator: "")
 }
