591a2b02bef1ded8a857968aa63d5f0a12bc6c3e
신동규 <donggyu9410@gmail.com>
2021-12-03 20:34:13 +0900

    feat: 124 나라 수정

--- practice/world124/contents.py
+++ practice/world124/contents.py
@@ -1,27 +1,24 @@
+answer = ''
+
+
 def solution(n: int):
-    answer = ''
+
     numbers = [4, 1, 2]
 
     def recursive(n: int):
         global answer
         result = int(n / 3)
+
         left = int(n % 3)
         value = str(numbers[left])
         answer = value + answer
 
-        if value == 4:
+        if value == '4':
+
             result -= 1
 
-        if result > 0:
+        if result >= 1:
             recursive(result)
 
     recursive(n)
     return answer
-
-
-def test():
-    print(solution(4), 11)
-    # print(solution(10), 41)
-
-
-test()
