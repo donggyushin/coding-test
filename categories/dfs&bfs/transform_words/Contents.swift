302fee71dcbfe4cbca96cfb6ca63a9e7254f6f0d
신동규 <donggyu9410@gmail.com>
2021-12-04 17:33:45 +0900

    refactor: 단어변환

--- categories/dfs&bfs/transform_words/Contents.swift
+++ categories/dfs&bfs/transform_words/Contents.swift
@@ -1,39 +1,37 @@
 import Foundation
 
-func is_change_able(word1: String, word2: String) -> Bool {
+func can_change(word1: String, word2: String) -> Bool {
     var count = 0
-    for (c1, c2) in zip(word1, word2) {
-        if c1 != c2 { count += 1 }
+    for (item1, item2) in zip(word1, word2) {
+        if item1 != item2 { count += 1 }
+        if count >= 2 {
+            return false
+        }
     }
-    return count <= 1
+    return count == 1
 }
 
 func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
-    var answers: [Int] = []
-    var words = words
-    words.insert(begin, at: 0)
+    let words = [begin] + words
+    var answer: Int?
+    let visited: [Bool] = .init(repeating: false, count: words.count)
     
-    func dfs(current: String, visited: [Bool], i: Int, count: Int) {
+    func bfs(i: Int, visited: [Bool], count: Int) {
+        let current_word = words[i]
+        if current_word == target {
+            answer = min(answer ?? words.count, count)
+            return
+        }
         var visited = visited
         visited[i] = true
-        let filtered = words.enumerated().filter({ is_change_able(word1: $1, word2: current) && visited[$0] == false })
-        var go_on = true
-        for (_, element) in filtered {
-            if element == target {
-                go_on = false
-                break
-            }
-        }
-        if go_on {
-            filtered.forEach {
-                dfs(current: words[$0.offset], visited: visited, i: $0.offset, count: count + 1)
-            }
-        } else {
-            answers.append(count + 1)
-        }
+        if visited.first(where: { $0 == false }) == nil { return }
+        
+        words.enumerated().filter({ visited[$0] == false && can_change(word1: current_word, word2: $1) }).forEach({
+            bfs(i: $0.offset, visited: visited, count: count + 1)
+        })
     }
     
-    dfs(current: begin, visited: words.map { _ in false }, i: 0, count: 0)
+    bfs(i: 0, visited: visited, count: 0)
     
-    return answers.min() ?? 0
+    return answer ?? 0
 }
